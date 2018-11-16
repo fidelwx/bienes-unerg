0f4cefa3d86c6c97469e9514ffd34fb41b9ebf94
fidelwx <fidel_herrera128@hotmail.com>
2018-11-16 13:02:53 -0430

    coreccion input

--- modal/registro_productos.php
+++ modal/registro_productos.php
@@ -60,9 +60,9 @@
 			</div>
 
 			<div class="form-group">
-				<label for="img" class="col-sm-3 control-label">Stock</label>
+				<label for="img" class="col-sm-3 control-label"">Img</label>
 				<div class="col-sm-8">
-				  <input type="file" class="form-control" id="img" name="img" placeholder="Foto del Producto">
+				  <input type="file" class="form-control" id="img" name="img" placeholder="Foto del Producto" accept="image/*">
 				</div>
 			</div>
 
